5cfca30491237a5c4b6fbbe8599ec778817e5eaa
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-11 12:37:04 +0300

    Made dodger move

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger= document.getElementById("dodger");
+function moveDodgerLeft(){
+    const leftNumbers=dodger.style.left.replace("px","");
+    const left=parseInt(leftNumbers,10);
+
+    if (left>0){
+        dodger.style.left=`${left-5}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers=dodger.style.left.replace("px","");
+    const left=parseInt(leftNumbers,10);
+
+    if (left>=0){
+        dodger.style.left=`${left+5}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    if(e.key==="ArrowLeft"){
+        moveDodgerLeft();
+        console.log("moved left");
+        console.log(dodger.style.left)
+
+    }
+    else if(e.key==="ArrowRight"){
+        moveDodgerRight();
+        console.log("moved right");
+        console.log(dodger.style.left)
+    }
+});
